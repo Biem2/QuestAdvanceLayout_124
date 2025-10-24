987db25c734d6b88852126034f73a4540cc5446d
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-10-24 14:40:16 +0700

    menambahkan Spacer dan Card dengan tinggi 160dp, lebar penuh, padding 12dp, serta warna latar DarkGray

--- app/src/main/java/com/example/praktikum4/ui.kt
+++ app/src/main/java/com/example/praktikum4/ui.kt
@@ -46,3 +46,14 @@ fun ActivitasPertama(modifier: Modifier){
             text = stringResource(id = R.string.univ),
             fontSize = 22.sp
     )
+    Spacer(modifier = Modifier.height(height = 25.dp))
+
+    Card(
+        modifier = Modifier
+            .height(160.dp)
+            .fillMaxWidth(fraction = 1f)
+            .padding(all = 12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.DarkGray
+        )
+    ){
