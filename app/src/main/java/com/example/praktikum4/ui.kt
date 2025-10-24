c954bc83569c6ebf856eb41fff1588df1a0628d8
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-10-24 14:45:41 +0700

    menambahkan Text alamat dengan ukuran font 20sp, warna kuning, dan padding atas 10dp

--- app/src/main/java/com/example/praktikum4/ui.kt
+++ app/src/main/java/com/example/praktikum4/ui.kt
@@ -98,3 +98,12 @@ fun ActivitasPertama(modifier: Modifier){
             containerColor = Color.Blue
     )
 ){
+        Text (
+        stringResource(R.string.alamat),
+        fontSize = 20.sp,
+        color = Color.Yellow,
+        modifier = Modifier.padding(top = 10.dp)
+    )
+    }
+}
+}
